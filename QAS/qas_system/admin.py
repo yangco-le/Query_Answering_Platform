503445f6ddd27f40b87475472b3501351f5b2c98
yangco-le <yanglily@sjtu.edu.cn>
2020-04-13 11:55:37 +0800

    建立数据库，构建简单admin界面，加入一些数据

--- QAS/qas_system/admin.py
+++ QAS/qas_system/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import User, Subject, Question, Comment
+
+admin.site.register(User)
+admin.site.register(Subject)
+admin.site.register(Question)
+admin.site.register(Comment)
--- QAS/qas_system/models.py
+++ QAS/qas_system/models.py
@@ -9,7 +9,10 @@ class User(models.Model):
     属性：用户名，密码
     '''
     user_name = models.CharField(verbose_name='用户名', max_length=30, blank=False)
-    password = models.CharField(verbose_name='密码', maxlength=30, blank=False)
+    password = models.CharField(verbose_name='密码', max_length=30, blank=False)
+
+    def __str__(self):
+        return self.user_name
 
 
 class Subject(models.Model):
