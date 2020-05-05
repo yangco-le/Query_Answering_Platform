40412d30c1f33513fdba4d243930f088e6f72377
huanghsly <lyslyslys9@126.com>
2020-05-05 16:22:38 +0800

    huanghaishi

--- QAS/qas_system/urls.py
+++ QAS/qas_system/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     path('search_keyword/', views.search_keyword, name='search_keyword'),  # 按关键字搜索问题
     path('userpage/<int:id>', views.userpage),  # 用户主页
     path('userpage/edit/<int:id>', views.userpage_edit, name='user_edit'),  # 个人信息编辑
-    path('userpage/related_discuss/<int:id>', views.userpage_related_discuss),  # 用户主页
+    path('userpage/related_discuss/<int:id>', views.userpage_related_discuss),  # 查看参与的讨论
 ]
