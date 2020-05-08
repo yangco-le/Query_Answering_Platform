54bc4dcab9015585d232276b08b09ae2e9779580
unknown <852406797@qq.com>
2020-05-09 01:25:06 +0800

    完成注册页面

--- QAS/qas_system/views.py
+++ QAS/qas_system/views.py
@@ -374,8 +374,48 @@ def user_login(request):
 
 
 def user_register(request):
-    pass
-    return render(request, 'register.html')
+    '''
+    用户注册
+    尹俊同
+    '''
+    if request.session.get('is_login', None):
+        return redirect('/qas_system/')
+
+    if request.method == 'POST':
+        register_form = forms.UserRegisterForm(request.POST)
+        message = "请检查填写的内容！"
+        if register_form.is_valid():
+            username = register_form.cleaned_data.get('username')
+            password1 = register_form.cleaned_data.get('password1')
+            password2 = register_form.cleaned_data.get('password2')
+            email = register_form.cleaned_data.get('email')
+            sex = register_form.cleaned_data.get('sex')
+
+            if password1 != password2:
+                message = '两次输入的密码不同！'
+                return render(request, 'register.html', locals())
+            else:
+                same_name_user = models.User.objects.filter(user_name=username)
+                if same_name_user:
+                    message = '用户名已经存在'
+                    return render(request, 'register.html', locals())
+                same_email_user = models.User.objects.filter(email=email)
+                if same_email_user:
+                    message = '该邮箱已经被注册了！'
+                    return render(request, 'register.html', locals())
+
+                new_user = models.User()
+                new_user.user_name = username
+                new_user.password = password1
+                new_user.email = email
+                new_user.sex = sex
+                new_user.save()
+
+                return redirect('/qas_system/login/')
+        else:
+            return render(request, 'register.html', locals())
+    register_form = forms.UserRegisterForm()
+    return render(request, 'register.html', locals())
 
 
 def user_logout(request):
