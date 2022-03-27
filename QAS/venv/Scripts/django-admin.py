e4c366b181021b9dab8230bf60d981f5e109d9ca
Yang Li <61114414+yangco-le@users.noreply.github.com>
2022-03-27 17:16:39 +0800

    Delete QAS/venv directory

--- QAS/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\24339\Desktop\repos\query_answering_system\QAS\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
